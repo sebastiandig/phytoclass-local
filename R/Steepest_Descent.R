a8b2ccc571bc607773659b54969e182d6d33a450
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-09 19:59:09 -0400

    add modified version of Steepest_Descent

--- R/Steepest_Descent.R
+++ R/Steepest_Descent.R
@@ -35,3 +35,54 @@ Steepest_Descent <- function(Fmat, place, S, cm, num.loops){
   }
   return(F.new)
 }
+
+
+# corrected for minimise_elements_2
+Steepest_Descent <- function(Fmat, place, S, cm, num.loops) {
+  loop <- 1
+  F.new <- NNLS_MF(Fmat, S, cm)
+  F.initial <- F.new
+  for (i in 1:num.loops) { # should always be small. It would be nice to allow the
+    # F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
+    F.new <- Minimise_elements_2(F.initial[[1]], place, S, cm, .num = 1, .fac_rr = 1)
+    loop <- loop + 1
+    loop_2 <- 1
+ 
+    while (F.new[[2]] > F.initial[[2]]) {
+      loop_2 <- loop_2 + 1
+
+      # determines which loop and changes minimize based on loop 
+      # 0 - 5 : 1
+      # 6 - 10: 2
+      # 11+   : 3
+      fac_rr_x <- findInterval(i, c(0, 5, 10), left.open = TRUE)
+      F.new <- Minimise_elements_2(
+        F.initial[[1]], place, S, cm,
+        .num = fac_rr_x,
+        .fac_rr = fac_rr_x
+      )
+
+      #
+      # # F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
+      # F.new <- Minimise_elements_2(F.initial[[1]], place, S, cm, .num = 3, .fac_rr = 3)
+      #
+      # if (loop_2 > 5) {
+      #   # F.new <- Minimise_elements1(F.initial[[1]], place, S, cm)
+      #   F.new  <- Minimise_elements_2(F.initial[[1]], place, S, cm, .num = 1, .fac_rr = 1)
+      #
+      # } # If it doesn't work the first time, it randomises at a lower rate
+      # if (loop_2 > 10) {
+      #   # F.new <- Minimise_elements2(F.initial[[1]], place, S, cm)
+      #   F.new <- Minimise_elements_2(F.initial[[1]], place, S, cm, .num = 2, .fac_rr = 2)
+      #
+      # } # and again,
+
+      #     if (loop_2 > 20){F.new <- Minimise_elements2(F.new[[1]])}
+      if (loop_2 > 100) {
+        break
+      } # it will continue for 100 itertions, and then stop
+    }
+    F.initial <- F.new
+  }
+  return(F.new)
+}
