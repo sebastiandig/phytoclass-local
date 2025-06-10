2d8dc6f31982f702dccd77825feb98af0ff4008e
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-09 23:52:43 -0400

    style and rm `Metrics` dependency

--- R/NNLS_MF.R
+++ R/NNLS_MF.R
@@ -2,7 +2,7 @@
 #' pigments and pigment ratios, to attain an estimate of phytoplankton 
 #' class abundances.
 #'
-#' @param Fn   Pigment to Chl a matrix
+#' @param Fn  Pigment to Chl a matrix
 #' @param S   Sample data matrix – a matrix of pigment samples
 #' @param cm  Weights for each column
 #'
@@ -21,22 +21,54 @@
 #' cm <- Bounded_weights(Snew, weight.upper.bound = 30)
 #' NNLS_MF(Fnew, Snew, cm)
 #'
-NNLS_MF <- function(Fn, S, cm=NULL){
-    if (is.null(cm)) {
-    cm <- as.vector(rep(1,ncol(S)))
+NNLS_MF <- function(Fn, S, cm = NULL) {
+  if (is.null(cm)) {
+    cm <- as.vector(rep(1, ncol(S)))
   }
 
-
-  b <- crossprod(t(Weight_error(Fn, cm)),t(Weight_error(S, cm)))
-  C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn, cm))),
-                          b, 
-                          cd_maxit = 1000,cd_tol =1e-8 ))
-  C_new2 <- as.matrix(C_new2)
-  Cn.s2 <- rowSums(C_new2)
-  Cn2 <- C_new2/Cn.s2 #Row sums to one
-  Cn2 <- as.matrix(Cn2)
+  Fn_wt_err <- t(Weight_error(Fn, cm))
+  S_wt_err  <- t(Weight_error(S, cm))
+  
+  b       <- crossprod(Fn_wt_err, S_wt_err)
+  Fn_prod <- crossprod(Fn_wt_err)
+  C_new2  <- 
+    RcppML::nnls(
+      Fn_prod,
+      b,
+      cd_maxit = 1000, 
+      cd_tol = 1e-8
+  )
+  C_new2  <- t(C_new2)
+  
+  # C_new2        <- as.matrix(C_new2)
+  # Cn.s2         <- rowSums(C_new2)
+  # Cn2           <- C_new2 / Cn.s2 
+  # Cn2           <- as.matrix(Cn2)
+  Cn2 <- Normalise_S(C_new2) # Row sums to one
+  
   colnames(Cn2) <- rownames(Fn)
-  error <- Metrics::rmse((S),C_new2%*%(Fn))
-  return(list("F matrix " = Fn, "RMSE" = error,"C matrix" = Cn2))
+  # error         <- Metrics::rmse(S, C_new2 %*% Fn)
+  error         <- sqrt(mean((S - C_new2 %*% Fn)^2)) # RMSE
+  return(list("F matrix" = Fn, "RMSE" = error, "C matrix" = Cn2))
 }
+
+# # fix `F matrix ` to `F matrix`
+# NNLS_MF <- function(Fn, S, cm=NULL){
+#     if (is.null(cm)) {
+#     cm <- as.vector(rep(1,ncol(S)))
+#   }
+# 
+# 
+#   b <- crossprod(t(Weight_error(Fn, cm)),t(Weight_error(S, cm)))
+#   C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn, cm))),
+#                           b, 
+#                           cd_maxit = 1000,cd_tol =1e-8 ))
+#   C_new2 <- as.matrix(C_new2)
+#   Cn.s2 <- rowSums(C_new2)
+#   Cn2 <- C_new2/Cn.s2 #Row sums to one
+#   Cn2 <- as.matrix(Cn2)
+#   colnames(Cn2) <- rownames(Fn)
+#   error <- Metrics::rmse((S),C_new2%*%(Fn))
+#   return(list("F matrix" = Fn, "RMSE" = error,"C matrix" = Cn2))
+# }
 
