b7ea0c380c9c9885bfe542e576077c826cb3f3d1
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-09 23:37:10 -0400

    rm commented code

--- R/Steepest_Descent.R
+++ R/Steepest_Descent.R
@@ -23,7 +23,7 @@ Steepest_Descent <- function(Fmat, place, S, cm, num.loops) {
     while (F.new[[2]] > F.initial[[2]]) {
       loop_2 <- loop_2 + 1
 
-      # determines which loop and changes minimize based on loop 
+      # determines which loop and changes minimize based on loop iteration
       # 0 - 5 : 1
       # 6 - 10: 2
       # 11+   : 3
@@ -35,36 +35,11 @@ Steepest_Descent <- function(Fmat, place, S, cm, num.loops) {
       )
 
       if (loop_2 > 100) {
+        # it will continue for 100 iterations, and then stop
         break
-      } # it will continue for 100 itertions, and then stop
+      } 
     }
     F.initial <- F.new
   }
   return(F.new)
 }
-
-
-# Steepest_Descent <- function(Fmat, place, S, cm, num.loops){ 
-#   loop <- 1
-#   F.new <- NNLS_MF(Fmat, S, cm)
-#   F.initial <- F.new
-#   for (i in 1:num.loops){ #should always be small. It would be nice to allow the 
-#     F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
-#     loop = loop +1
-#     # print(loop)
-#     loop_2 <- 1
-#     # print(F.new[[2]])
-#     while (F.new[[2]] > F.initial[[2]]) {
-#       loop_2 = loop_2+1
-#       #print(loop_2)
-#       F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
-#       if (loop_2 > 5){F.new <-  Minimise_elements1(F.initial[[1]], place,S,cm)} # If it doesn't work the first time, it randomises at a lower rate
-#       if (loop_2 > 10){F.new <- Minimise_elements2(F.initial[[1]], place,S,cm)} # and again, 
-#       
-#       #     if (loop_2 > 20){F.new <- Minimise_elements2(F.new[[1]])}
-#       if (loop_2 > 100){break} # it will continue for 100 itertions, and then stop
-#     }
-#     F.initial <- F.new
-#   }
-#   return(F.new)
-# }
