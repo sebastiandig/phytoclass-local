5196bef728a2e27b576a4443e2bf6dd74cd44428
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-09 23:20:29 -0400

    stylizing and minor update

--- R/Replace_Rand.R
+++ R/Replace_Rand.R
@@ -1,24 +1,48 @@
 #' Select the new F matrix element with lowest error in the steepest
 #' descent algorithm. 
 #' 
+#' Randomizes one non-zero pigment ratio and runs NNLS and detects if NNLS error 
+#' is lower than previous. Then, records TRUE/FALSE.
+#' 
 #' @keywords internal
 #'
-#' @param Fmat   xx
-#' @param i xx
-#' @param S   xx
-#' @param cm   xx
-#' @param min.scaler   xx
-#' @param max.scaler  xx
+#' @param Fmat A matrix of pigment ratios (F matrix)
+#' @param i    Element number to randomize in Fmat
+#' @param S    A matrix of samples (rows) and pigments (colums)
+#' @param cm   A vector of weights
+#' @param min.scaler  A numeric for minimum scale values
+#' @param max.scaler  A numeric for maximum scale values
 #'
-#' @return
+#' @return A list with results from `NNLS_MF()` and logical if reduces error
 #'
 #' @examples
-Replace_Rand <- function(Fmat, i, S, cm, min.scaler, max.scaler){
-  # randomise first element of matrix  
-  F.new <- as.matrix(replace(Fmat[[1]], i, Randomise_elements(Fmat[[1]][i], min.scaler, max.scaler))) 
-  F.new <- NNLS_MF(F.new, S, cm)
-  # Which elements decrease the error? Store the location of the elements that decrease it   
-  v <- which(F.new[[2]] < Fmat[[2]])
-  res <- c(F.new,v) 
+Replace_Rand <- function(Fmat, i, S, cm, min.scaler, max.scaler) {
+
+  # randomise non-zero elements
+  Fmat_1   <- Fmat[[1]] # extract Fmat as "F matrix" = Fn, "RMSE" = error, "C matrix" = Cn2
+  new_rand <- Randomise_elements(Fmat_1[i], min.scaler, max.scaler) # randomize one element
+  F_new    <- replace(Fmat_1, i, new_rand)
+  F_new    <- as.matrix(F_new)
+  F_new    <- NNLS_MF(F_new, S, cm)
+
+  # Which elements decrease the error? Store the location of the elements that decrease it
+  # v   <- which(F_new[[2]] < Fmat[[2]]) # true or false
+  v   <- F_new[[2]] < Fmat[[2]] # compare RMSE from previous, if better TRUE
+  res <- c(F_new, v)
+
   return(res)
 }
+
+
+
+# Replace_Rand <- function(Fmat, i, S, cm, min.scaler, max.scaler){
+#   # randomise first element of matrix
+#   F.new <- as.matrix(replace(Fmat[[1]], i, Randomise_elements(Fmat[[1]][i], min.scaler, max.scaler)))
+#   F.new <- NNLS_MF(F.new, S, cm)
+#   # Which elements decrease the error? Store the location of the elements that decrease it
+#   v <- which(F.new[[2]] < Fmat[[2]])
+#   res <- c(F.new,v)
+#   return(res)
+# }
+# 
+
