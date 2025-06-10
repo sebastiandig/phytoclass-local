2e29963591e290cf65b4686ed2d73f326c91d2d4
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-09 23:53:51 -0400

    rm commented lines

--- R/NNLS_MF.R
+++ R/NNLS_MF.R
@@ -38,37 +38,11 @@ NNLS_MF <- function(Fn, S, cm = NULL) {
       cd_maxit = 1000, 
       cd_tol = 1e-8
   )
-  C_new2  <- t(C_new2)
-  
-  # C_new2        <- as.matrix(C_new2)
-  # Cn.s2         <- rowSums(C_new2)
-  # Cn2           <- C_new2 / Cn.s2 
-  # Cn2           <- as.matrix(Cn2)
-  Cn2 <- Normalise_S(C_new2) # Row sums to one
+  C_new2 <- t(C_new2)
+  Cn2    <- Normalise_S(C_new2) # Row sums to one
   
   colnames(Cn2) <- rownames(Fn)
-  # error         <- Metrics::rmse(S, C_new2 %*% Fn)
   error         <- sqrt(mean((S - C_new2 %*% Fn)^2)) # RMSE
+  
   return(list("F matrix" = Fn, "RMSE" = error, "C matrix" = Cn2))
 }
-
-# # fix `F matrix ` to `F matrix`
-# NNLS_MF <- function(Fn, S, cm=NULL){
-#     if (is.null(cm)) {
-#     cm <- as.vector(rep(1,ncol(S)))
-#   }
-# 
-# 
-#   b <- crossprod(t(Weight_error(Fn, cm)),t(Weight_error(S, cm)))
-#   C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn, cm))),
-#                           b, 
-#                           cd_maxit = 1000,cd_tol =1e-8 ))
-#   C_new2 <- as.matrix(C_new2)
-#   Cn.s2 <- rowSums(C_new2)
-#   Cn2 <- C_new2/Cn.s2 #Row sums to one
-#   Cn2 <- as.matrix(Cn2)
-#   colnames(Cn2) <- rownames(Fn)
-#   error <- Metrics::rmse((S),C_new2%*%(Fn))
-#   return(list("F matrix" = Fn, "RMSE" = error,"C matrix" = Cn2))
-# }
-
