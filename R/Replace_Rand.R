8f83c1b77a354383b1bde8823ea910afd62d2820
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-09 23:20:52 -0400

    rm commented lines

--- R/Replace_Rand.R
+++ R/Replace_Rand.R
@@ -32,17 +32,3 @@ Replace_Rand <- function(Fmat, i, S, cm, min.scaler, max.scaler) {
 
   return(res)
 }
-
-
-
-# Replace_Rand <- function(Fmat, i, S, cm, min.scaler, max.scaler){
-#   # randomise first element of matrix
-#   F.new <- as.matrix(replace(Fmat[[1]], i, Randomise_elements(Fmat[[1]][i], min.scaler, max.scaler)))
-#   F.new <- NNLS_MF(F.new, S, cm)
-#   # Which elements decrease the error? Store the location of the elements that decrease it
-#   v <- which(F.new[[2]] < Fmat[[2]])
-#   res <- c(F.new,v)
-#   return(res)
-# }
-# 
-
